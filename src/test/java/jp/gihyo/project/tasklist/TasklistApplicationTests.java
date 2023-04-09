5782bb0d991d61d0a3006f7be14e23762324bf7d
yosh-0827 <yuyuyu.1648@gmail.com>
2023-04-09 19:31:34 +0900

    test

---
+++ src/test/java/jp/gihyo/project/tasklist/TasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.project.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
