17f683e0801ba779c9f4eff2117638d5cf974668
yosh-0827 <yuyuyu.1648@gmail.com>
2023-04-09 19:40:22 +0900

    Pushてすと

--- src/main/java/jp/gihyo/project/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/project/tasklist/HomeRestController.java
@@ -9,7 +9,7 @@ public class HomeRestController {
     @RequestMapping(value = "/resthello")
     String hello() {
         return """
-                Hello.aaa
+                Hello.
                 It works!
                 現在時刻は%sです。
                 """.formatted(LocalDateTime.now());
