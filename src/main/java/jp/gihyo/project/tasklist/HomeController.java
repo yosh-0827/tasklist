b7d0a9828a626f968226c5d3c54940a33011a8c1
yosh-0827 <yuyuyu.1648@gmail.com>
2023-04-11 22:34:42 +0900

    ResponsebodyアノテーションをコメントアウトすることでHTMLが表示された

--- src/main/java/jp/gihyo/project/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/project/tasklist/HomeController.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Controller
 public class HomeController {
     @RequestMapping("/hello")       //"~/hello"というパスに対するエンドポイントとして指定
-    @ResponseBody                   //戻り値としてビューを表すオブジェクトを返す
+    //@ResponseBody                   //戻り値としてビューを表すオブジェクトを返す
     String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
         return "hello";             //ビューを構成するHTMLはテンプレートエンジンが作成してくるので対応するビュー名を拡張子除いて返す
