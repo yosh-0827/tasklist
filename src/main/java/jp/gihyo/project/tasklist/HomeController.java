0f89588eacf152468f5b791c3cd28f95f7ca79be
yosh-0827 <yuyuyu.1648@gmail.com>
2023-04-12 21:11:35 +0900

    タスク登録、一覧のエンドポイント作成

--- src/main/java/jp/gihyo/project/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/project/tasklist/HomeController.java
@@ -1,10 +1,15 @@
 package jp.gihyo.project.tasklist;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
@@ -13,5 +18,25 @@ public class HomeController {
     String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
         return "hello";             //ビューを構成するHTMLはテンプレートエンジンが作成してくるので対応するビュー名を拡張子除いて返す
+    }
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+    //タスク一覧表示用エンドポイント(home.htmlに対応)
+    @GetMapping("/list")
+    String listItems(Model model) {
+        model.addAttribute("taskList", taskItems);   //キー：taskList, バリュー：taskItem
+        return "home";
+    }
+
+    //タスク追加のエンドポイント
+    @GetMapping("/add")
+    String addItem(@RequestParam("task") String task,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0,8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "redirect:/list";
     }
 }
